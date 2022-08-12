fcb232a4e7b66c1211f622f2399ac81220ded7ac
Paul Krucylak <paulkrucylak@gmail.com>
2022-08-12 11:01:40 -0500

    create function to play best of 5 rounds

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 //create function to generate random computer input (either rock, paper, or scissors)
+let winner = "";
+
 function getComputerChoice() {
     // generate random number
     const num = Math.floor(Math.random()*3);
@@ -22,19 +24,22 @@ function getComputerChoice() {
 // create a function to play thorugh a round of rock paper scissor and returns win or loss statement
 function playRound(playerSelection,computerSelection) {
     // make player choice case insensitive
-    const playerChoice = playerSelection.toUpperCase();
-    const computerChoice = computerSelection;
-
+    let playerChoice = playerSelection.toUpperCase();
+    let computerChoice = computerSelection;
+    winner = ""
     // if player choice does not match ROCK PAPER or SCISSORS, return error message
     if (playerChoice != "ROCK" && playerChoice != "PAPER" && playerChoice != "SCISSORS") {
-       return "This is not a valid input, please try again";
+        console.log("This is not a valid input, please try again");
+        return playRound(prompt("Please input a valid action this time."),getComputerChoice());
     }
     // compare player choice and computer choice
     else {
         if (playerChoice == "ROCK" && computerChoice == "SCISSORS" || playerChoice == "PAPER" && computerChoice == "ROCK" || playerChoice == "SCISSORS" && computerChoice == "PAPER") {
+            winner = "player";
             return "Player Choice: " +playerChoice + " Computer Choice: "+computerChoice +" Congratulations, you won!";
         }
         else if (playerChoice == "ROCK" && computerChoice == "PAPER" || playerChoice == "PAPER" && computerChoice == "SCISSORS" || playerChoice == "SCISSORS" && computerChoice == "ROCK") {
+            winner = "computer";
             return "Player Choice: " +playerChoice + " Computer Choice: "+computerChoice +" Sorry, you have lost";
         }
         else {
@@ -46,19 +51,54 @@ function playRound(playerSelection,computerSelection) {
 
 }
 
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+//const playerSelection = "rock";
+//const computerSelection = getComputerChoice();
+//console.log(playRound(playerSelection, computerSelection));
 
-// create loop to play 5 games
-
+// create five round game function
+function game() {
     // define variables to track scores
-
+    let compWins = 0;
+    let playerWins = 0;
     
-    // end loop and display winner if player or computer win 3 times
+    // create loop to play 5 games
+    for(i=0;i<5;i++){
+    
+        // end loop and display winner if player or computer win 3 times
+        if (compWins >= 3) {
+            return "The Computer wins!";
+        }
+        else if (playerWins >= 3) {
+            return "You have won!";
+        }
+        else {
+            // run one game of rock paper scissorsg
+            console.log("Round "+(i+1))
+            console.log(playRound(prompt("Do you pick rock, paper, or scissors?"),getComputerChoice()));
+            if (winner == "computer") {
+                compWins = compWins + 1;
+            }
+            else if (winner == "player") {
+                playerWins = playerWins + 1;
+            }
+            console.log("Player Score: "+playerWins + "   Computer Score: "+compWins)
+            if (i==4) {
+                if (compWins == 3){
+                    return "The Computer wins!";
+                }
+                else if (playerWins == 3) {
+                    return "You have won!";
+                }
+                else{
+                    return "No winners";
+                }
+            }
 
+        
 
-    // run one game of rock paper scissors
+        }
 
-
-    // add to winners score
+    }
+      
+}
+    
